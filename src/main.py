62d2f833763d1269fa541618d65e2378eab48238
Christoph Jüngling <chris@juengling-edv.de>
2025-03-04 09:11:12 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from sub1.submain1 import main as main1
+from sub2.submain2 import main as main2
+
+
+def main():
+    print('I am the main file.')
+    main1()
+    main2()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sub1/submain1.py
@@ -0,0 +1,3 @@
+def main():
+    print('I am the main from sub1.')
+
